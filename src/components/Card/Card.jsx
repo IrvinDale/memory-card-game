4f9f9f536af6414dbb9dc1c35b9cad61c53c174b
IrvinDale <irvindale24@gmail.com>
2025-03-25 16:04:00 -0400

    created new game logic and added new styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,33 +1,20 @@
 import React, { useState, useCallback } from "react";
-import Loader from "../Loader";
 import styles from "./Card.module.scss";
 
-const Card = React.memo(function Card({ imgUrl, imageId, categoryName, processTurn }) {
-  const [isLoading, setIsLoading] = useState(true);
 
-  const handleImageLoad = useCallback(() => {
-    setIsLoading(false);
-  }, []);
-
-  const handleClick = useCallback(() => {
-    processTurn(imageId);
-  }, [processTurn, imageId]);
-
+function Card({ imgUrl, isFlipped, processTurn }) {
   return (
-    <div className={styles.container} onClick={handleClick}>
-      {isLoading && (
-        <div className={styles.loaderContainer}>
-          <Loader message="Loading..." />
-        </div>
-      )}
+    <div
+      className={`${styles.container} ${isFlipped ? styles.flipped : ""}`}
+      onClick={processTurn}
+    >
       <img
         src={imgUrl}
-        alt={categoryName}
-        onLoad={handleImageLoad}
-        className={`${styles.image} ${isLoading ? styles.hidden : ''}`}
+        alt="Card"
+        className={`${styles.image} ${!isFlipped ? styles.hidden : ""}`}
       />
     </div>
   );
-});
+}
 
 export default Card;

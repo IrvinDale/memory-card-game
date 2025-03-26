489b00b511055fc06c24bfdf680a9a33528e6ff1
IrvinDale <irvindale24@gmail.com>
2025-03-25 23:29:59 -0400

    updated styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,7 @@ import styles from "./Header.module.scss";
 export default function Header() {
   return (
     <header className={styles.heading}>
-      <h2>Memory Card Game</h2>
-
+      <h2>Memory Game</h2>
     </header>
   );
 }
